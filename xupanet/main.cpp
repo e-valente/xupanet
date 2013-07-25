229d46571d8857e9f833cfd624d12fac1c3f55a8
emanuelvalente <emanuelvalente@gmail.com>
2013-07-25 02:43:44 -0300

    center of screen fixed

--- xupanet/main.cpp
+++ xupanet/main.cpp
@@ -1,11 +1,33 @@
 #include "xupanet.h"
 #include <QApplication>
+#include <QDesktopWidget>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+    int screenWidth, screenHeight;
+    int w, h;
+    int x, y;
+
     QApplication a(argc, argv);
-    XupaNet w;
-    w.show();
+    XupaNet window;
+    QDesktopWidget *desktop = QApplication::desktop();
+
+    screenHeight = desktop->height();
+    screenWidth = desktop->width();
+
+   // qDebug() <<"dimensoes: altura: " << screenHeight << "largura: " << screenWidth;
+
+    w = 1200;
+    h = 1000;
+
+    y = (screenHeight - h)/2;
+    x = (screenWidth - w) /2;
+
+    window.resize(w, h);
+    window.move(x, y);
+
+    window.show();
     
     return a.exec();
 }
